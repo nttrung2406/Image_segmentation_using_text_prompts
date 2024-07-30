f4ad7d85d43ba97b30cada56b68db8d58ed5f4ba
NguyenThanhTrung <105348335+nttrung2406@users.noreply.github.com>
2024-07-30 16:09:46 +0700

    Create DiceLoss.py

---
+++ weights_finetune/custom/DiceLoss.py
@@ -0,0 +1,20 @@
+from torch import nn
+from torch.nn import functional as F
+
+class DiceLoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(DiceLoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+        
+       
+
+        inputs = F.sigmoid(inputs)       
+        
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        intersection = (inputs * targets).sum()                            
+        dice_loss = 1 - (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)  
+        
+        return dice_loss
